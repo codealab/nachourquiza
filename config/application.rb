5811f5f2debc40f9b42ea93cfae672b56352e09f
El Flaco <redasterix89@gmail.com>
2014-08-13 19:20:36 -0500

    Production configuration

--- config/application.rb
+++ config/application.rb
@@ -14,10 +14,12 @@ module Nachourquiza
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.time_zone = 'Guadalajara'
+    config.i18n.default_locale = :es
+    config.assets.initialize_on_precompile = false
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
