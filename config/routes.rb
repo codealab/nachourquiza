64dcdd3f6cc911eddfc2a18b2f6894a4bf8f5100
El Flaco <redasterix89@gmail.com>
2014-08-26 11:36:48 -0500

    users resources

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :lectures
   resources :password_resets
   resources :payments
-  
+  resources :users
   resources :users do
     resources :payments
     resources :spots
